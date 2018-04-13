2525a0f7fb8c78e5aff80fba403fd9a6012aad61
Chris Mullins <cleemullins@users.noreply.github.com>
2018-04-13 16:32:22 -0700

    BotStateSet

---
+++ libraries/Microsoft.Bot.Builder.Core.Extensions/BotStateSet.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Core.Extensions
+{
+    /// <summary>
+    /// Middleware that will call `read()` and `write()` in parallel 
+    /// on multiple `BotState` instances.
+    /// </summary>
+    public class BotStateSet : IMiddleware
+    {
+        private IList<BotState> 
+        public Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
